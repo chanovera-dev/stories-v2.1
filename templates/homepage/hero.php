68ab07af2caf51c4540beeaaa2d02c9cd424a6b8
Chano Vera <github@chanovera.com>
2025-12-31 20:27:45 -0600

    hero section width ACF fields

--- templates/homepage/hero.php
+++ templates/homepage/hero.php
@@ -1,12 +1,36 @@
+<?php
+/**
+ * Template part for the 'hero' section on the homepage.
+ *
+ * This section displays the hero section.
+ *
+ * @package Stories
+ * @version 2.1
+ */
+
+$span = get_field('hero_span');
+$title = get_field('hero_title');
+$subtitle = get_field('hero_subtitle');
+$phone = get_field('hero_phone');
+$phone_label = get_field('hero_phone_label');
+$contact = get_field('hero_section_contact_link');
+$contact_label = get_field('hero_section_contact_link_label');
+?>
 <section id="hero" class="block">
     <div class="content hero--content heading">
-        <span>Gestión de crisis</span>
-        <h1 class="page-title">Recupera el control <i>de tu narrativa</i></h1>
-        <p>Gestionamos crisis de alto perfil, control de daños y protección reputacional. Te acompañamos hasta
-            recuperar el control de la narrativa.</p>
+        <span><?php echo $span; ?></span>
+        <?php echo $title; ?>
+        <?php echo $subtitle; ?>
         <div class="cta-buttons">
-            <button class="btn"><?= stories_get_icon('phone'); ?>Es una emergencia</button>
-            <button class="btn hollow">Diagnóstico confidencial</button>
+            <button class="btn" onclick="window.location.href='<?= 'tel:' . $phone ?>'">
+                <?=
+                    stories_get_icon('phone') .
+                    $phone_label;
+                ?>
+            </button>
+            <button class="btn hollow" onclick="window.location.href='<?= $contact ?>'">
+                <?= $contact_label; ?>
+            </button>
         </div>
     </div>
 </section>
